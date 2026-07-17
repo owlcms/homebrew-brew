d60ed8f70842ba9fa5f5440a140afe319076c943
Jean-François Lamy <jf@jflamy.dev>
2026-07-17 10:23:54 -0400

    versioned

---
+++ Casks/controlpanel@3.4.0.rb
@@ -0,0 +1,27 @@
+cask "controlpanel@3.4.0" do
+  version "3.4.0"
+
+  on_arm do
+    sha256 "952be7657799ea77d8f59258418ee5168d7a76a8674375656d20f8347e70bca6"
+
+    url "https://github.com/owlcms/owlcms-controlpanel/releases/download/#{version}/macOS_OWLCMS.dmg",
+        verified: "github.com/owlcms/owlcms-controlpanel/"
+  end
+
+  on_intel do
+    sha256 "cb7fcc4c4119bd8b8ac89403a058064b7f42b055e149b403a3fc6821a8569a73"
+
+    url "https://github.com/owlcms/owlcms-controlpanel/releases/download/#{version}/macOS_Intel_OWLCMS.dmg",
+        verified: "github.com/owlcms/owlcms-controlpanel/"
+  end
+
+  name "OWLCMS Control Panel 3.4.0"
+  desc "Control panel for OWLCMS weightlifting competitions"
+  homepage "https://github.com/owlcms/owlcms-controlpanel"
+
+  app "owlcms.app"
+
+  postflight do
+    system_command "/usr/bin/xattr", args: ["-cr", "#{appdir}/owlcms.app"]
+  end
+end
---
+++ Casks/controlpanel@3.5.0-rc02.rb
@@ -0,0 +1,27 @@
+cask "controlpanel@3.5.0-rc02" do
+  version "3.5.0-rc02"
+
+  on_arm do
+    sha256 "606dcffbfffd255a8625855afa0d79ef346427712e9c49cd091deb4ecc8860f7"
+
+    url "https://github.com/owlcms/owlcms-controlpanel/releases/download/#{version}/macOS_OWLCMS.dmg",
+        verified: "github.com/owlcms/owlcms-controlpanel/"
+  end
+
+  on_intel do
+    sha256 "6739242a35c8d977d3d78e92a061bdf8431aa712cc5d20d60c3552b82ed3779f"
+
+    url "https://github.com/owlcms/owlcms-controlpanel/releases/download/#{version}/macOS_Intel_OWLCMS.dmg",
+        verified: "github.com/owlcms/owlcms-controlpanel/"
+  end
+
+  name "OWLCMS Control Panel 3.5.0-rc02"
+  desc "Control panel for OWLCMS weightlifting competitions"
+  homepage "https://github.com/owlcms/owlcms-controlpanel"
+
+  app "owlcms.app"
+
+  postflight do
+    system_command "/usr/bin/xattr", args: ["-cr", "#{appdir}/owlcms.app"]
+  end
+end
